f44dc1373e20ba401494af81e1db4bb17833347c
David Lazic <lazic.ddavid@gmail.com>
2025-08-10 15:53:23 +0200

    added imageURL

--- formlist/formlist.js
+++ formlist/formlist.js
@@ -2,3 +2,4 @@ const form = document.getElementById("birthdayForm");
 const list = document.getElementById("addedList");
 const nameInput = document.getElementById("name");
 const lastNameInput = document.getElementById("last-name");
+const imageUrlInput = document.getElementById("imageUrl");
