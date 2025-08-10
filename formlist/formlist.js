11c633c8bd65dae6483f84f03b982cb5db76f0d3
David Lazic <lazic.ddavid@gmail.com>
2025-08-10 15:53:55 +0200

    Added let

--- formlist/formlist.js
+++ formlist/formlist.js
@@ -3,3 +3,5 @@ const list = document.getElementById("addedList");
 const nameInput = document.getElementById("name");
 const lastNameInput = document.getElementById("last-name");
 const imageUrlInput = document.getElementById("imageUrl");
+
+let birthdays = [];
